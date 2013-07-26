d04b1c1865ac605de4a7bbc0c1a9bcbe90ae7450
pascalc <pascal.chevrel@free.fr>
2013-07-26 18:21:37 +0200

    factorize the initialization code for the app and put it in inc/init.php

---
+++ inc/init.php
@@ -0,0 +1,23 @@
+<?php
+
+/* server shortcuts */
+$aproot    = __DIR__ . '/../';
+$libs      = $aproot . 'libs/';
+$conf      = $aproot . 'config/';
+$views     = $aproot . 'views/';
+$templates = $aproot . 'templates/';
+
+/* functions needed to manipulate .lang files */
+require_once $libs . 'l10n_moz.class.php';
+require_once $libs . 'functions.inc.php';
+
+/* app-wide variables */
+require $conf . 'locales.inc.php';
+require $conf . 'sources.inc.php';
+
+/* user provided variables */
+$filename = (isset($_GET['file']))    ? secureText($_GET['file'])    : '';
+$locale   = (isset($_GET['locale']))  ? secureText($_GET['locale'])  : ''; // which locale are we analysing? No default
+$website  = (isset($_GET['website'])) ? secureText($_GET['website']) : ''; // which website are we looking at?
+$action   = (isset($_GET['action']))  ? secureText($_GET['action'])  : '';
+$serial   = isset($_GET['serial']);  // Do we want serialize data for the webdashboard?
--- index.php
+++ index.php
@@ -1,27 +1,8 @@
 <?php
 
-/* server shortcuts */
-$called    = true;
-$aproot    = __DIR__ . '/';
-$libs      = $aproot . 'libs/';
-$conf      = $aproot . 'config/';
-$views     = $aproot . 'views/';
-$templates = $aproot . 'templates/';
+require_once __DIR__ . '/inc/init.php';
 
-/* functions needed to manipulate .lang files */
-require_once $libs . 'l10n_moz.class.php';
-require_once $libs . 'functions.inc.php';
-
-/* app-wide variables */
-require $conf . 'sources.inc.php';   // websites definition, needs locales.inc.php
-
-/* user provided variables */
-$filename = (isset($_GET['file']))    ? secureText($_GET['file'])    : 'main.lang';
-$locale   = (isset($_GET['locale']))  ? secureText($_GET['locale'])  : ''; // which locale are we analysing? No default
-$website  = (isset($_GET['website'])) ? secureText($_GET['website']) : ''; // which website are we looking at?
-$action   = (isset($_GET['action']))  ? secureText($_GET['action'])  : '';
-$serial   = isset($_GET['serial']);  // Do we want serialize data for the webdashboard?
-$case     = 0;
+$case = 0;
 
 if ($locale == '' && $action == '') {
     /* case 1: no locale is requested, we display links to all locales */
