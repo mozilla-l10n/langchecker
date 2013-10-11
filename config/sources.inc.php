484a685573b7e6285214b3bf98bfae38537f0a97
pascalc <pascal.chevrel@free.fr>
2013-10-11 08:44:07 +0200

    update locales

--- config/sources.inc.php
+++ config/sources.inc.php
@@ -141,7 +141,8 @@ $langfiles_subsets = [
              'de', 'el', 'hu', 'it', 'lv', 'lt', 'nb-NO', 'pl', 'pt-PT',
              'ro', 'sk', 'sl', 'es-ES', 'sv-SE'],
         'firefox/new.lang'                      => $mozilla,
-        'firefox/channel.lang'                  => ['cs', 'de', 'es-MX', 'fr', 'he', 'sk'],
+        'firefox/channel.lang'                  =>
+            ['bg', 'cs', 'de', 'es-MX', 'fr', 'he', 'sk'],
         'firefox/os/index.lang'                 =>
             ['cs', 'de', 'el', 'es-ES', 'et', 'fr', 'hu', 'ja', 'it', 'pl', 'pt-BR', 'ro', 'sr'],
         'firefox/partners/index.lang'           =>
