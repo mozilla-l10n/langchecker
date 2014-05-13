8d4da0049165b3f280801efaf4b2b77c1fd51766
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-05-13 08:05:03 +0200

    Add history-details to es-CL

--- config/sources.inc.php
+++ config/sources.inc.php
@@ -315,7 +315,7 @@ $langfiles_subsets = [
              'pa-IN', 'pl', 'pt-BR', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'ta', 'tr',
              'zh-CN', 'zh-TW'],
         'mozorg/about/history-details.lang'     =>
-            ['ca', 'de', 'eu', 'fr', 'gl', 'it', 'pa-IN', 'sk', 'sq', 'zh-TW'],
+            ['ca', 'de', 'es-CL', 'eu', 'fr', 'gl', 'it', 'pa-IN', 'sk', 'sq', 'zh-TW'],
         'mozorg/contribute.lang'                =>
             ['ar', 'bs', 'cs', 'cy', 'de', 'el', 'es-AR', 'es-CL', 'es-ES',
              'es-MX', 'fr', 'hr', 'fy-NL', 'he', 'hi-IN', 'hr', 'id',
