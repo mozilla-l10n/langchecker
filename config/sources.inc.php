3ae3b752ed2702334085578fd05f70643d4c7170
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-05-15 19:21:56 +0200

    Add es-MX to locales we import from Pootle

--- config/sources.inc.php
+++ config/sources.inc.php
@@ -16,8 +16,8 @@ if (!isset($_SERVER['SERVER_NAME'])) {
 require __DIR__ . '/settings.inc.php';
 include __DIR__ . '/adu.inc.php';
 
-$locamotion_locales = ['ach', 'af', 'cy', 'dsb', 'en-ZA', 'ff', 'gd', 'hi-IN', 'hsb',
-                       'km', 'ku', 'ms', 'my', 'oc', 'son', 'ur', 'vi', 'xh'];
+$locamotion_locales = ['ach', 'af', 'cy', 'dsb', 'en-ZA', 'es-MX', 'ff', 'gd', 'hi-IN',
+                       'hsb', 'km', 'ku', 'ms', 'my', 'oc', 'son', 'ur', 'vi', 'xh'];
 
 /*
     Locales removed from locamotion import because of commits on SVN
