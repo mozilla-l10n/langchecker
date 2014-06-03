fbc51ec81cbb65bfdeb1399813e09716ca81c4ab
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-06-03 18:03:09 +0200

    Fix pt -> pt-PT in firefox_updater

--- config/sources.inc.php
+++ config/sources.inc.php
@@ -176,7 +176,7 @@ $snippets_main_locales = ['cs', 'de', 'el', 'es-ES', 'fr', 'hu', 'id', 'it', 'ja
 $addons_locales = ['de', 'es-ES', 'fr', 'id', 'it', 'ja', 'pt-BR'];
 
 $firefox_updater_locales = ['ar', 'cs', 'de', 'el', 'es', 'fi', 'fr', 'hu', 'id', 'it', 'ja',
-                            'nl', 'pl', 'pt-BR', 'pt', 'ru', 'sl', 'sv', 'th', 'tr', 'vi',
+                            'nl', 'pl', 'pt-BR', 'pt-PT', 'ru', 'sl', 'sv', 'th', 'tr', 'vi',
                             'zh-CN', 'zh-TW'];
 
 $sites =
