788fcdb3a0451e1828e66f368da0df2a0f547b2b
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-07-30 08:45:54 +0200

    List pages: add anchors and possibility to display only one website

--- views/listpages.inc.php
+++ views/listpages.inc.php
@@ -6,8 +6,17 @@ namespace Langchecker;
 <?php
 
 $htmloutput = '';
-foreach ($sites as $site_index => $current_website) {
-    $htmloutput .= "\n\t<h2>" . Project::getWebsiteName($current_website) . "</h2>\n";
+$displayed_sites = [];
+
+if ($website != '' && isset($sites[$website])) {
+    $displayed_sites[] = $sites[$website];
+} else {
+    $displayed_sites = $sites;
+}
+
+foreach ($displayed_sites as $site_index => $current_website) {
+    $websitename = Project::getWebsiteName($current_website);
+    $htmloutput .= "\n\t<h2 id='{$websitename}'><a href='#{$websitename}'>{$websitename}</a></h2>\n";
     $htmloutput .= "\t<table class='listpages'>
         <thead>
             <tr>
