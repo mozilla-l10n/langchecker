73299995ef7d431031277baeb266bcdf95b30fce
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-08-01 15:35:50 +0200

    Import po: ignore translations starting with ";" Add tests for import local po file.

--- tests/units/Langchecker/LangManager.php
+++ tests/units/Langchecker/LangManager.php
@@ -3,6 +3,7 @@ namespace tests\units\Langchecker;
 
 use atoum;
 use Langchecker\LangManager as _LangManager;
+use Langchecker\DotLangParser as _DotLangParser;
 
 require_once __DIR__ . '/../bootstrap.php';
 
@@ -86,13 +87,12 @@ class LangManager extends atoum\test
         // Total number of strings
         $this
             ->integer(count($strings))
-                ->isEqualTo(7);
+                ->isEqualTo(9);
 
         // Identical string
         $this
             ->string($strings['Download'])
                 ->isEqualTo('Download {ok}');
-
 
         // Single line string
         $this
@@ -103,5 +103,50 @@ class LangManager extends atoum\test
         $this
             ->string($strings['As a non-profit, we’re free to innovate on your behalf without any pressure to compromise.'])
                 ->isEqualTo('Como una organización sin fines de lucro, somos libres de innovar en tu nombre sin presión o compromiso alguno.');
+    }
+
+    public function testImportLocalPoFile()
+    {
+        $po_filepath = TEST_FILES . 'gettext/test.po';
+        $lang_filepath = TEST_FILES . 'gettext/test.lang';
+        $obj = new _LangManager();
+
+        $locale_data = _DotLangParser::parseFile($lang_filepath, false);
+        $po_import = $obj->importLocalPoFile($po_filepath, $locale_data, false);
+
+        // I should have imported data
+        $this
+            ->boolean($po_import['imported'])
+                ->isTrue();
+
+        // I should have one error
+        $this
+            ->integer(count($po_import['errors']))
+                ->isEqualTo(1);
+
+        // Test one translated string
+        $this
+            ->string($po_import['strings']['Innovating <span>for you</span>'])
+                ->isEqualTo('¡Inovando <span>para ti!</span>');
+
+        // Test one translated string in both lang and po file (second has precedence)
+        $this
+            ->string($po_import['strings']['Fast, flexible, <span>secure</span>'])
+                ->isEqualTo('Rápido, flexible, seguro');
+
+        // Empty translation in .po file, existing in .lang file
+        $this
+            ->string($po_import['strings']['Empty'])
+                ->isEqualTo('test');
+
+        // Test one identical string
+        $this
+            ->string($po_import['strings']['Download'])
+                ->isEqualTo('Download {ok}');
+
+        // Test one translation starting with ; (should be ignored)
+        $this
+            ->string($po_import['strings']['Download test'])
+                ->isEqualTo('Download test');
     }
 }
