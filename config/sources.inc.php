8c6ed1ce29a78c65fc4d9773bd689f2f65895177
Francesco Lodolo (:flod) <flod@lodolo.net>
2014-08-20 17:56:33 +0200

    Add bn-BD to Locamotion locales

--- config/sources.inc.php
+++ config/sources.inc.php
@@ -19,9 +19,9 @@ if (!isset($_SERVER['SERVER_NAME'])) {
 require __DIR__ . '/settings.inc.php';
 include __DIR__ . '/adu.inc.php';
 
-$locamotion_locales = ['ach', 'af', 'cy', 'dsb', 'en-ZA', 'es-MX', 'ff', 'gd', 'hi-IN',
-                       'hsb', 'km', 'ku', 'ms', 'my', 'oc', 'son', 'ta', 'ur', 'uz',
-                       'vi', 'xh'];
+$locamotion_locales = ['ach', 'af', 'bn-BD', 'cy', 'dsb', 'en-ZA', 'es-MX', 'ff',
+                       'gd', 'hi-IN', 'hsb', 'km', 'ku', 'ms', 'my', 'oc', 'son',
+                       'ta', 'ur', 'uz', 'vi', 'xh'];
 
 /*
     Locales removed from locamotion import because of commits on SVN
