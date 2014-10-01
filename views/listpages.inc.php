bb98f2f76cc8f9505fdc32105781324f334c6548
pascalc <pascal.chevrel@free.fr>
2014-10-01 11:28:48 +0200

    Issue #174: show lang files stats (number of files/strings/words) on listpages view
    {review nits

--- views/listpages.inc.php
+++ views/listpages.inc.php
@@ -19,7 +19,7 @@ foreach ($displayed_sites as $site_index => $current_website) {
     $website_data_source = Project::getWebsiteDataType($current_website);
 
     if ($website_data_source == 'lang') {
-        $table_headers = "<th>Filename</th><th>Status</th><th>Translate</th>";
+        $table_headers = "<th>Filename</th><th>Status</th><th>Translations</th><th>Strings</th><th>Words</th>";
     } else {
         $table_headers = "<th>Filename</th>\n<th>Status</th>\n\n";
     }
@@ -29,12 +29,31 @@ foreach ($displayed_sites as $site_index => $current_website) {
             <tr>{$table_headers}</tr>
         </thead>
         <tbody>\n";
+
+    // Totals to display in the table footer
+    $total_strings = $total_words = $total_files = 0;
+
     foreach (Project::getWebsiteFiles($current_website) as $current_filename) {
         if ($website_data_source == 'lang') {
+            $reference_locale = Project::getReferenceLocale($current_website);
+            $ref_lang_file = LangManager::loadSource($current_website, $reference_locale, $current_filename);
+
+            $get_words = function($item) {
+                return str_word_count(strip_tags($item));
+            };
+
+            $nb_words = array_sum(array_map($get_words, $ref_lang_file['strings']));
+            $nb_strings = count($ref_lang_file['strings']);
+            $total_strings += $nb_strings;
+            $total_words += $nb_words;
+            $total_files++;
+
             $htmloutput .= "<tr>\n" .
                            "  <td>{$current_filename}</td>\n" .
                            "  <td><a href='?locale=all&amp;website={$site_index}&amp;file={$current_filename}'>Status</a></td>\n" .
-                           "  <td><a href='?website={$site_index}&amp;file={$current_filename}&amp;action=translate&amp;show'>Translate</a></td>\n" .
+                           "  <td><a href='?website={$site_index}&amp;file={$current_filename}&amp;action=translate&amp;show'>Show</a></td>\n" .
+                           "  <td>{$nb_strings}</td>\n" .
+                           "  <td>{$nb_words}</td>\n" .
                            "</tr>\n";
         } else {
             $htmloutput .= "<tr>\n" .
@@ -43,6 +62,18 @@ foreach ($displayed_sites as $site_index => $current_website) {
                            "</tr>\n";
         }
     }
+
+    $total_files .= ($total_files != 1) ? ' files' : ' file';
+
+    if ($website_data_source == 'lang') {
+        $htmloutput .= "<tr class=\"tabletotals\">\n" .
+                       "  <th colspan=\"2\">Total</th>\n" .
+                       "  <td>{$total_files}</td>\n" .
+                       "  <td>{$total_strings}</td>\n" .
+                       "  <td>{$total_words}</td>\n" .
+                       "</tr>\n";
+    }
+
     $htmloutput .= "    </tbody>\n</table>\n\n";
 }
 
