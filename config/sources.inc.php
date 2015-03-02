3a45aa68adbe5f534d70e0e6d05fb280ee616490
Francesco Lodolo <flod@lodolo.net>
2015-03-02 21:45:13 +0100

    Add 'lt' to Locamotion locales

--- config/sources.inc.php
+++ config/sources.inc.php
@@ -23,7 +23,7 @@ include __DIR__ . '/adu.inc.php';
 
 $locamotion_locales = [
     'ach', 'af', 'bn-BD', 'ca', 'cy', 'dsb', 'ee', 'en-ZA', 'es-MX', 'ff',
-    'gd', 'ha', 'hi-IN', 'hr', 'hsb', 'ig', 'km', 'ln', 'ms', 'my',
+    'gd', 'ha', 'hi-IN', 'hr', 'hsb', 'ig', 'km', 'ln', 'lt', 'ms', 'my',
     'oc', 'sat', 'si', 'son', 'sw', 'ta', 'ur', 'uz', 'vi', 'wo',
     'xh', 'yo', 'zu',
 ];
