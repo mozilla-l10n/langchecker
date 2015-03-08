788ffb62829bb3380e97e8f7b0d4a660e93a7abc
Francesco Lodolo <flod@lodolo.net>
2015-03-08 14:36:04 +0000

    Use Pootle for Breton

--- config/sources.inc.php
+++ config/sources.inc.php
@@ -22,7 +22,7 @@ require __DIR__ . '/settings.inc.php';
 include __DIR__ . '/adu.inc.php';
 
 $locamotion_locales = [
-    'ach', 'af', 'bn-BD', 'ca', 'cy', 'dsb', 'ee', 'en-ZA', 'es-MX', 'ff',
+    'ach', 'af', 'bn-BD', 'br', 'ca', 'cy', 'dsb', 'ee', 'en-ZA', 'es-MX', 'ff',
     'gd', 'ha', 'hi-IN', 'hr', 'hsb', 'ig', 'km', 'ln', 'lt', 'ms', 'my',
     'oc', 'sat', 'si', 'son', 'sw', 'ta', 'ur', 'uz', 'vi', 'wo',
     'xh', 'yo', 'zu',
