7b75cb1fe1360a9aa458de9db448325dcc4f5022
Francesco Lodolo <flod@lodolo.net>
2015-03-31 14:46:28 +0200

    Fix syntax error
    Missing quote :-\

--- config/sources.inc.php
+++ config/sources.inc.php
@@ -756,7 +756,7 @@ $langfiles_subsets = [
         'firefox/dnt.lang'                      =>
             [
                 'cs', 'cy', 'de', 'dsb', 'es-CL', 'fr', 'hsb', 'it', 'ja',
-                'lt', 'nl ,'pt-BR', 'sat', 'sk', 'son', 'sq', 'sv-SE', 'uk',
+                'lt', 'nl' ,'pt-BR', 'sat', 'sk', 'son', 'sq', 'sv-SE', 'uk',
                 'uz', 'zh-TW',
             ],
         'firefox/family/index.lang'             => $firefox_desktop,
