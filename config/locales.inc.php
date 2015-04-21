646d9d6d8a76bdd7c886de1b06bf7e5dd1fd95fd
Francesco Lodolo (:flod) <flod@lodolo.net>
2015-04-21 07:05:34 +0200

    Bug 1156479 - Start tracking Tagalog (tl) for Fx OS

--- config/locales.inc.php
+++ config/locales.inc.php
@@ -13,7 +13,7 @@ $mozilla = [
     'my', 'nb-NO', 'nl', 'nn-NO', 'oc', 'or', 'pa-IN', 'pl',
     'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'sat', 'si', 'sk',
     'sl', 'son', 'sq', 'sr', 'sr-Latn', 'sv-SE', 'sw', 'ta',
-    'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 'wo', 'xh',
+    'te', 'th', 'tl', 'tr', 'uk', 'ur', 'uz', 'vi', 'wo', 'xh',
     'yo', 'zh-CN', 'zh-TW', 'zu',
 ];
 sort($mozilla);
