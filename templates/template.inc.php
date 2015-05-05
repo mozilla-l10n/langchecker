b53ccdb8bfaa4bc6f834bef37c5cbc7739ac09c7
Pascal Chevrel <pascal.chevrel@free.fr>
2015-05-05 11:30:21 +0200

    Issue #312: improve performance of error page - faster Utils::isUTF8() by giving the path of the Magic mime file on the server - store parsed English reference files in a static property instead of reparsing them every time - in DotLangParser::ParseFile() avoid reparsing the whole file in the main loop for file metadata once we are already parsing strings - Utils::starstWith() && Utils::leftStrip() have specific cases for searching ; and # with less calculation (on the error page we call these functions up to 2 million times) - new DotLangParser::getMetaTags() utility function to return the list of tags we use in lang files - moved parsing of reference metadata to a private DotLangParser::extractReferenceMetaData() property to make parseFile() more readable - added an optional DEBUG constant in settings.inc.php, if set to True, the template will output perf statistics

--- templates/template.inc.php
+++ templates/template.inc.php
@@ -17,8 +17,16 @@
 	</div>
 </div>
 <?php
-    echo "<!-- Elapsed time (s): " . round((microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]), 4) . " -->\n";
-    echo "<!-- Memory usage (MB): " . round(memory_get_peak_usage(true) / (1024 * 1024), 2) . " -->\n";
+$time   = 'Elapsed time (s): ' . round((microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]), 4);
+$memory = 'Memory usage (MB): ' . round(memory_get_peak_usage(true) / (1024 * 1024), 2);
+
+print "<!-- " . $time . " -->\n";
+print "<!-- " . $memory . " -->\n";
+
+if (defined('DEBUG') && DEBUG) {
+    error_log($time);
+    error_log($memory);
+}
 ?>
 </body>
 </html>
