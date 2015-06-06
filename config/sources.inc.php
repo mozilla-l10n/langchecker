7425b7a2706bb5b4d6f82eaa148d1d499eb60e92
Francesco Lodolo <flod@lodolo.net>
2015-06-06 15:16:11 +0300

    Add Latvian to Pootle locales

--- config/sources.inc.php
+++ config/sources.inc.php
@@ -31,7 +31,7 @@ if (is_file(__DIR__ . '/adi.inc.php')) {
 $locamotion_locales = [
     'ach', 'af', 'bm', 'br', 'ca', 'cy', 'dsb', 'ee',
     'en-ZA', 'es-MX', 'ff', 'fi', 'ga-IE', 'gd', 'ha', 'hi-IN',
-    'hr', 'hsb', 'ig', 'km', 'kok', 'ks', 'ln', 'lt', 'ms',
+    'hr', 'hsb', 'ig', 'km', 'kok', 'ks', 'ln', 'lt', 'lv', 'ms',
     'my', 'nn-NO', 'oc', 'pt-PT', 'sat', 'si', 'son', 'sw', 'ta',
     'tl', 'tn', 'ur', 'uz', 'vi', 'wo', 'xh', 'yo', 'zu',
 ];
