b902cf5d98a632ff6c0c7e8183c073ec2dfec90d
Francesco Lodolo <flod@lodolo.net>
2015-11-23 15:37:27 +0100

    Remove 'fa' from Pootle locales

--- app/config/locales.inc.php
+++ app/config/locales.inc.php
@@ -54,7 +54,7 @@ $newsletter_locales = ['de', 'es-ES', 'fr', 'hu', 'id', 'pl', 'pt-BR', 'ru'];
 // List of locales working on Pootle
 $locamotion_locales = [
     'ach', 'af', 'bm', 'bn-BD', 'br', 'ca', 'cak', 'cy', 'dsb',
-    'ee', 'en-ZA', 'es-MX', 'fa', 'ff', 'fi', 'ga-IE', 'gd',
+    'ee', 'en-ZA', 'es-MX', 'ff', 'fi', 'ga-IE', 'gd',
     'gn', 'ha', 'hi-IN', 'hr', 'hsb', 'ig', 'kk', 'km', 'kok',
     'ks', 'ln', 'lo', 'lt', 'lv', 'mai', 'mg', 'ml', 'mr', 'ms',
     'my', 'nn-NO', 'oc', 'or', 'sat', 'si', 'son', 'sw', 'ta',
