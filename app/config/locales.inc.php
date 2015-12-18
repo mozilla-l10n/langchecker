a0bca0c69072d5c6245188589b820a74fa00de9f
Francesco Lodolo <flod@lodolo.net>
2015-12-18 15:23:50 +0100

    Remove 'gd' from Pootle locales

--- app/config/locales.inc.php
+++ app/config/locales.inc.php
@@ -53,12 +53,13 @@ $newsletter_locales = ['de', 'es-ES', 'fr', 'hu', 'id', 'pl', 'pt-BR', 'ru'];
 
 // List of locales working on Pootle
 $locamotion_locales = [
-    'ach', 'af', 'bm', 'bn-BD', 'br', 'ca', 'cak', 'cy', 'dsb',
-    'ee', 'en-ZA', 'es-MX', 'ff', 'fi', 'ga-IE', 'gd',
-    'gn', 'ha', 'hi-IN', 'hr', 'hsb', 'ig', 'kk', 'km', 'kok',
-    'ks', 'ln', 'lo', 'lt', 'lv', 'mai', 'mg', 'mr', 'ms',
-    'my', 'nn-NO', 'oc', 'or', 'sat', 'son', 'sw', 'ta', 'te',
-    'tl', 'tn', 'tsz', 'ur', 'vi', 'wo', 'xh', 'yo', 'zu',
+    'ach', 'af', 'bm', 'bn-BD', 'br', 'ca', 'cak', 'cy',
+    'dsb', 'ee', 'en-ZA', 'es-MX', 'ff', 'fi', 'ga-IE',
+    'gn', 'ha', 'hi-IN', 'hr', 'hsb', 'ig', 'kk', 'km',
+    'kok', 'ks', 'ln', 'lo', 'lt', 'lv', 'mai', 'mg',
+    'mr', 'ms', 'my', 'nn-NO', 'oc', 'or', 'sat', 'son',
+    'sw', 'ta', 'te', 'tl', 'tn', 'tsz', 'ur', 'vi',
+    'wo', 'xh', 'yo', 'zu',
 ];
 
 /* List of Mozilla's locales supported by SurveyGizmo.
