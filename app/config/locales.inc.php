605a67de29336e82e53921272a304765d27025f8
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-02-25 20:24:25 +0100

    Fix supported locales for landing pages (Android, iOS)

--- app/config/locales.inc.php
+++ app/config/locales.inc.php
@@ -53,6 +53,54 @@ $firefox_locales = array_diff(
 $firefox_desktop_android = array_merge($firefox_locales, $fennec_locales);
 
 /*
+    source: https://l10n.mozilla-community.org/~flod/webstatus/api/?product=firefox-ios
+    translations are at: https://github.com/mozilla-l10n/firefoxios-l10n/
+    For iOS we used the locale code es for Spanish from Spain, that was a
+    mistake, this is why I changed it to es-ES in the array below, otherwise
+    the Spanish team would have to work in the es-ES folder for Android and
+    the es folder for iOS
+    Make sure to update the store_l10n project when you update this list:
+    https://github.com/mozilla-l10n/stores_l10n/blob/15633f598a78357575630fdc235f9cbccc4c6ed3/app/classes/Stores/Project.php#L43
+*/
+$apple_store_target = [
+    'ar', 'az', 'bg', 'bn-IN', 'br', 'cs', 'cy', 'da', 'de', 'dsb', 'el',
+    'eo', 'es-ES', 'es-CL', 'es-MX', 'fa', 'fr', 'fy-NL', 'ga-IE',
+    'gd', 'gl', 'hsb', 'id', 'is', 'it', 'ja', 'kk', 'km', 'ko', 'lo', 'lt',
+    'lv', 'ml', 'ms', 'my', 'nb-NO', 'nl', 'nn-NO', 'or', 'pl', 'pt-BR',
+    'pt-PT', 'rm', 'ro', 'ru', 'sk', 'sl', 'son', 'sv-SE', 'th', 'tl', 'tr',
+    'uk', 'uz', 'zh-CN', 'zh-TW',
+];
+
+/*
+    Source : http://hg.mozilla.org/releases/mozilla-release/raw-file/tip/mobile/android/locales/maemo-locales
+    Source : http://hg.mozilla.org/releases/mozilla-beta/raw-file/tip/mobile/android/locales/maemo-locales
+    Source : http://hg.mozilla.org/releases/mozilla-aurora/raw-file/tip/mobile/android/locales/maemo-locales
+    When updating, make sure to update store_l10n project as well:
+    https://github.com/mozilla-l10n/stores_l10n/blob/15633f598a78357575630fdc235f9cbccc4c6ed3/app/classes/Stores/Project.php#L16
+*/
+$android_locales = [
+    'an', 'as', 'az', 'be', 'bn-IN', 'br', 'ca', 'cs', 'cy', 'da', 'de',
+    'dsb', 'en-GB', 'en-ZA', 'eo', 'es-AR', 'es-ES', 'es-MX', 'et', 'eu',
+    'fi', 'ff', 'fr', 'fy-NL', 'ga-IE', 'gd', 'gl', 'gu-IN', 'hi-IN', 'hr',
+    'hsb', 'hu', 'hy-AM', 'id', 'is', 'it', 'ja', 'kk', 'kn', 'ko', 'lt',
+    'lv', 'mai', 'ml', 'mr', 'ms', 'my', 'nb-NO', 'nl', 'or', 'pa-IN', 'pl',
+    'pt-BR', 'pt-PT', 'ro', 'ru', 'sk', 'sl', 'son', 'sq', 'sv-SE', 'ta',
+    'te', 'th', 'tr', 'uk', 'uz', 'zh-CN', 'zh-TW',
+];
+
+// List provided by Release-drivers, needs access to a Google Play publishing account
+$google_play_locales = [
+    'af', 'ar', 'be', 'bg', 'cs', 'ca', 'da', 'de', 'el', 'en-GB',
+    'es-MX', 'es-ES', 'et', 'fa', 'fi', 'fr', 'hi-IN', 'hu', 'hr',
+    'id', 'it', 'he', 'ja', 'ko', 'lt', 'lv', 'ms', 'nl', 'nb-NO',
+    'pl', 'pt-BR', 'pt-PT', 'rm', 'ro', 'ru', 'sk', 'sl', 'sr',
+    'sv-SE', 'sw', 'th', 'tr', 'uk', 'vi', 'zh-CN', 'zh-TW', 'zu',
+];
+
+// Locales that we do support and that Google Play supports too
+$google_play_target = array_intersect($android_locales, $google_play_locales);
+
+/*
     Thunderbird locales on Release channel
     Source: http://hg.mozilla.org/releases/comm-release/raw-file/tip/mail/locales/shipped-locales
 */
