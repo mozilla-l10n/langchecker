a82c6ca3df79ed6fb3b734a22d24968b34144434
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-03-30 11:25:58 +0200

    Add ESLInt configuration file and exclusion

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            2,
+            4
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "semi": [
+            2,
+            "always"
+        ],
+        "curly": [
+            2,
+            "all"
+        ],
+        "camelcase": [
+            2,
+            {
+                "properties": "always"
+            }
+        ],
+        "eqeqeq": [
+            2,
+            "smart"
+        ],
+        "one-var-declaration-per-line": [
+            2,
+            "always"
+        ],
+        "new-cap": 2
+    }
+};
