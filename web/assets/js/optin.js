24adcae2821e65b4b70e274870c7548c46350b23
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-03-30 11:15:40 +0200

    Fix ESLint errors in optin.js

--- web/assets/js/optin.js
+++ web/assets/js/optin.js
@@ -1,56 +1,56 @@
-function getSelectedPages() {
-    // Return array of selected pages
-    var checkboxes = document.getElementsByClassName('optin_checkbox');
-    var list = [];
-
-    for (var i = 0, n = checkboxes.length; i < n; i++) {
-        if (checkboxes[i].checked) {
-            list.push(checkboxes[i].value);
-        }
-    }
-
-    return list;
-};
-
-function toggleBugzillaButton (event) {
-    if (event.target.checked) {
-        // Make sure the button is enabled
-        bugzilla_button.removeAttribute('disabled');
-    } else {
-        // Check if the button should be disabled
-        var selected_pages = getSelectedPages();
-
-        if (selected_pages.length === 0) {
-            bugzilla_button.setAttribute('disabled', 'disabled');
-        }
-    }
-};
-
 window.onload = function() {
-    var bugzilla_button = document.getElementById('bugzilla_button');
-
     // Hide warning for disabled JavaScript
-    var noscript_warning = document.getElementById('noscript_warning');
-    noscript_warning.style.display = 'none';
+    var noScriptWarning = document.getElementById('noscript_warning');
+    noScriptWarning.style.display = 'none';
 
-    if (bugzilla_button) {
+    var bugzillaButton = document.getElementById('bugzilla_button');
+
+    function getSelectedPages() {
+        // Return array of selected pages
+        var checkboxes = document.getElementsByClassName('optin_checkbox');
+        var list = [];
+
+        for (var i = 0, n = checkboxes.length; i < n; i++) {
+            if (checkboxes[i].checked) {
+                list.push(checkboxes[i].value);
+            }
+        }
+
+        return list;
+    }
+
+    function toggleBugzillaButton (event) {
+        if (event.target.checked) {
+            // Make sure the button is enabled
+            bugzillaButton.removeAttribute('disabled');
+        } else {
+            // Check if the button should be disabled
+            var selectedPages = getSelectedPages();
+
+            if (selectedPages.length === 0) {
+                bugzillaButton.setAttribute('disabled', 'disabled');
+            }
+        }
+    }
+
+    if (bugzillaButton) {
         // Add listener only if the button is available,
         // i.e. there are pages available for opt-in
-        bugzilla_button.addEventListener('click', function() {
-            var base_url = document.getElementById('base_url');
+        bugzillaButton.addEventListener('click', function() {
+            var baseURL = document.getElementById('base_url');
             var locale = document.getElementById('locale_code');
             var message;
-            var selected_pages = getSelectedPages();
+            var selectedPages = getSelectedPages();
 
-            if (selected_pages.length > 0) {
+            if (selectedPages.length > 0) {
                 message = 'Please add ' + locale.value + ' to the list of ' +
                     'supported locales for the following files on ' +
                     'www.mozilla.org:\n';
-                for (var i = 0, n = selected_pages.length; i < n; i++) {
-                    message += '* ' + selected_pages[i] + '\n';
+                for (var i = 0, n = selectedPages.length; i < n; i++) {
+                    message += '* ' + selectedPages[i] + '\n';
                 }
-                var bugzilla_url = base_url.value + encodeURI(message);
-                var win = window.open(bugzilla_url, '_blank');
+                var bugzillaURL = baseURL.value + encodeURI(message);
+                var win = window.open(bugzillaURL, '_blank');
                 win.focus();
             } else {
                 alert('Please select at least one page to proceed.');
@@ -63,4 +63,4 @@ window.onload = function() {
             checkboxes[i].addEventListener('click', toggleBugzillaButton);
         }
     }
-}
+};
