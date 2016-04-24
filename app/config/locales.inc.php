a8ef8877517b838c7915be3f0c5c89de8b667256
Francesco Lodolo <flod@lodolo.net>
2016-04-24 13:09:42 +0200

    Add nb-NO to Locamotion locales

--- app/config/locales.inc.php
+++ app/config/locales.inc.php
@@ -135,12 +135,12 @@ $newsletter_locales = ['de', 'es-ES', 'fr', 'hu', 'id', 'pl', 'pt-BR', 'ru'];
 // List of locales working on Pootle
 $locamotion_locales = [
     'ach', 'af', 'bm', 'bn-BD', 'br', 'ca', 'cak', 'cy',
-    'dsb', 'ee', 'en-ZA', 'ff', 'ga-IE', 'gn',
-    'ha', 'hi-IN', 'hr', 'hsb', 'hto', 'ig', 'kk', 'km',
-    'kok', 'ks', 'ln', 'lo', 'lt', 'lv', 'mai', 'mg',
-    'mr', 'ms', 'my', 'ne-NP', 'nn-NO', 'oc', 'or', 'pbb',
-    'qvi', 'sat', 'son', 'sw', 'ta', 'tl', 'tn', 'trs',
-    'ur', 'vi', 'wo', 'xh', 'yo', 'zu',
+    'dsb', 'ee', 'en-ZA', 'ff', 'ga-IE', 'gn', 'ha',
+    'hi-IN', 'hr', 'hsb', 'hto', 'ig', 'kk', 'km', 'kok',
+    'ks', 'ln', 'lo', 'lt', 'lv', 'mai', 'mg', 'mr',
+    'ms', 'my', 'ne-NP', 'nb-NO', 'nn-NO', 'oc', 'or', 
+    'pbb', 'qvi', 'sat', 'son', 'sw', 'ta', 'tl', 'tn',
+    'trs', 'ur', 'vi', 'wo', 'xh', 'yo', 'zu',
 ];
 
 /*
