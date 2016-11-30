6d0b1e2966311e4f041aabe7914f3e0a3db82e5a
Théo Chevalier <theo.chevalier11@gmail.com>
2016-11-30 00:25:04 +0000

    fix arrays

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -795,8 +795,8 @@ $langfiles_subsets = [
 
     'engagement' =>
     [
-        'ads/ios_ads_nov2015.lang'     => ['de', 'fr'],
-        'ads/ios_android_apr2016.lang' => ['de', 'es-ES', 'fr', 'pl'],
+        'ads/ios_ads_nov2015.lang'             => ['de', 'fr'],
+        'ads/ios_android_apr2016.lang'         => ['de', 'es-ES', 'fr', 'pl'],
         'emails/2016/fundraising_email_1.lang' => ['de', 'es-ES', 'fr', 'it'],
         'emails/2016/fundraising_email_2.lang' => ['de', 'es-ES', 'fr', 'it'],
         'heartbeat/2016/sep2016.lang'  =>
