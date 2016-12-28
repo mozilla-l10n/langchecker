5a3f1978ca20853bc6b6ed7d62c1ee837773b91c
Francesco Lodolo (:flod) <flod@lodolo.net>
2016-12-28 07:25:06 +0100

    Silence output in GitHub webhook

--- web/github_hook.php
+++ web/github_hook.php
@@ -57,7 +57,8 @@ if (isset($_SERVER[$header])) {
 
         // Pull latest changes
         $log .= "Updating Git repository\n";
-        exec("git checkout $branch; git pull origin $branch");
+        exec("git checkout $branch >/dev/null 2>&1");
+        exec("git pull origin $branch >/dev/null 2>&1");
 
         // Delete cache
         $log .= "Removing existing cache\n";
