a96fcf667c1787bed135e8bbfc50e9ef14b0f26f
Peiying <pmo@mozilla.com>
2017-03-21 11:15:48 -0700

    Add firefox/all.lang; add new locales to IH subpages (#614)

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -111,6 +111,11 @@ $mozillaorg_lang = [
         'deadline'          => '2016-03-15',
         'supported_locales' => $mozillaorg,
     ],
+    'firefox/all.lang' => [
+        'deadline'          => '2017-04-04',
+        'priority'          => 1,
+        'supported_locales' => $firefox_desktop_android,
+    ],
     'firefox/android/index.lang' => [
         'flags' => [
             'opt-in' => ['all'],
@@ -406,7 +411,7 @@ $mozillaorg_lang = [
         ],
         'priority'          => 3,
         'supported_locales' => [
-            'cy', 'de', 'es-ES', 'fr', 'kab',
+            'af', 'cy', 'de', 'es-ES', 'fr', 'kab', 'pt-BR',
         ],
     ],
     'mozorg/internet-health/open-innovation.lang' => [
@@ -416,7 +421,7 @@ $mozillaorg_lang = [
         ],
         'priority'          => 3,
         'supported_locales' => [
-            'cy', 'de', 'es-ES', 'fr', 'kab',
+            'cy', 'de', 'es-ES', 'fr', 'kab', 'pt-BR',
         ],
     ],
     'mozorg/internet-health/privacy-security.lang' => [
