fe8c85e28ff897be94b60bfe6317eb63308fd85f
Francesco Lodolo <flod@lodolo.net>
2017-03-25 16:46:43 +0100

    Add Bosnian to Pootle locales

--- app/config/locales.inc.php
+++ app/config/locales.inc.php
@@ -71,7 +71,7 @@ $newsletter_locales = [
 
 // List of locales working on Pootle
 $locamotion_locales = [
-    'ach', 'af', 'bm', 'bn-BD', 'br', 'ca', 'cak', 'cy', 'ee', 'en-ZA', 'ff',
+    'ach', 'af', 'bm', 'bn-BD', 'br', 'bs', 'ca', 'cak', 'cy', 'ee', 'en-ZA', 'ff',
     'ga-IE', 'gn', 'ha', 'hi-IN', 'hto', 'ig', 'kk', 'ln', 'lt', 'lv', 'mai',
     'mg', 'nb-NO', 'ncj', 'ne-NP', 'nv', 'oc', 'or', 'pbb', 'qvi', 'son', 'sw',
     'ta', 'tn', 'trs', 'ur', 'wo', 'xh', 'yo', 'zam', 'zu',
