7dd3ab134114368383447f27aac355311ccf1f7a
Francesco Lodolo <flod@lodolo.net>
2017-08-03 09:01:01 +0200

    Remove bn-BD from Pootle

--- app/config/locales.inc.php
+++ app/config/locales.inc.php
@@ -73,7 +73,7 @@ $newsletter_locales = [
 
 // List of locales working on Pootle
 $locamotion_locales = [
-    'af', 'an', 'azz', 'bn-BD', 'br', 'bs', 'ca', 'cak', 'cy', 'en-ZA', 'ff', 'ga-IE',
+    'af', 'an', 'azz', 'br', 'bs', 'ca', 'cak', 'cy', 'en-ZA', 'ff', 'ga-IE',
     'gn', 'hi-IN', 'hto', 'lt', 'mai', 'ne-NP', 'nv', 'or', 'pai', 'pbb',
     'qvi', 'son', 'sw', 'ta', 'trs', 'ur', 'xh', 'zam', 'zu',
 ];
