99050cc1ad3052015f4f89cbcff1abacc7c37e50
Francesco Lodolo [:flod] <flodolo@mozilla.com>
2018-02-15 07:42:29 +0100

    Remove firefox/whatsnew_56

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -288,11 +288,6 @@ $mozillaorg_lang = [
        ],
        'supported_locales' => $firefox_locales,
     ],
-    'firefox/whatsnew_56.lang' => [
-        'deadline'          => '2017-09-26',
-        'priority'          => 1,
-        'supported_locales' => $firefox_locales,
-    ],
     'firefox/whatsnew_57.lang' => [
         'deadline'          => '2017-11-05',
         'priority'          => 1,
