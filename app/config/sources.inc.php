9e062d74b6e208d730d1473fb4255afab71fefa7
Théo Chevalier <theo.chevalier11@gmail.com>
2018-03-15 14:56:28 +0000

    Remove IHR from es, fr temporarily (#785)

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -767,7 +767,7 @@ $engagement_lang = [
     ],
     'emails/2018/ihr_launch.lang' => [
         'deadline'          => '2018-04-08',
-        'supported_locales' => ['de', 'es', 'fr'],
+        'supported_locales' => ['de'],
     ],
     'emails/2018/jan_mobile_app_updates.lang' => [
         'deadline'          => '2018-02-06',
