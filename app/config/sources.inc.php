3d944f320cab8f7c3b7d886aad737f4487b5bffe
Peiying <pmo@mozilla.com>
2018-06-21 13:50:09 +0800

    Newsletter per bug 1467252 (#822)

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -513,6 +513,11 @@ $mozillaorg_lang = [
     'mozorg/technology.lang' => [
         'supported_locales' => $mozillaorg,
     ],
+    'newsletter/opt-out-confirmation.lang' => [
+        'deadline'          => '2018-06-22',
+        'priority'          => 2,
+        'supported_locales' => ['de'],
+    ],
     'newsletter.lang' => [
         'priority'          => 1,
         'supported_locales' => $mozillaorg,
