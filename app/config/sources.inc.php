9bcf6cd3ffa4d0c3f9b016ad07004ea282f90ce4
Théo Chevalier <theo.chevalier11@gmail.com>
2019-02-20 14:01:45 +0000

    FB open letter follow up

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -841,6 +841,9 @@ $engagement_lang = [
     'emails/2019/fb_open_letter.lang' => [
         'supported_locales' => ['de', 'fr'],
     ],
+    'emails/2019/fb_open_letter_response.lang' => [
+        'supported_locales' => ['de', 'fr'],
+    ],
     'emails/2019/misinfo_survey.lang' => [
         'supported_locales' => ['de', 'fr'],
     ],
