ff2b25f2a151891a89f3404bd390acdb1ce41a07
Francesco Lodolo <flodolo@mozilla.com>
2019-05-28 21:39:32 +0200

    Add locale detection on opt-in page (#884)

--- app/classes/Langchecker/Utils.php
+++ app/classes/Langchecker/Utils.php
@@ -298,4 +298,52 @@ class Utils
 
         return $fallback;
     }
+
+    /**
+     * Try to get the best supported locale from HTTP_ACCEPT_LANGUAGE header
+     *
+     * @param array  $available_locales Available locales
+     * @param string $fallback_locale   Fallback locale
+     * @param string $header            HTTP_ACCEPT_LANGUAGE header
+     *
+     * @return string Best supported locale code
+     */
+    public static function detectLocale($available_locales = [], $fallback_locale = 'en-US', $header = '')
+    {
+        $accept_languages = [];
+        if ($header == '') {
+            // Read the header from the server, if available
+            $header = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
+        }
+        // Source: http://www.thefutureoftheweb.com/blog/use-accept-language-header
+        if ($header != '') {
+            // Break up string into pieces (languages and q factors)
+            preg_match_all(
+                '/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i',
+                $header,
+                $lang_parse
+            );
+            if (count($lang_parse[1])) {
+                // Create a list like "en" => 0.8
+                $accept_languages = array_combine($lang_parse[1], $lang_parse[4]);
+                // Set default to 1 for any without q factor
+                foreach ($accept_languages as $accept_locale => $val) {
+                    if ($val === '') {
+                        $accept_languages[$accept_locale] = 1;
+                    }
+                }
+                // Sort list based on value
+                arsort($accept_languages, SORT_NUMERIC);
+            }
+        }
+
+        // Check if any of the locales is available
+        $intersection = array_values(array_intersect(array_keys($accept_languages), $available_locales));
+        if (! isset($intersection[0])) {
+            // Accept-Language doesn't include any supported locale
+            return $fallback_locale;
+        } else {
+            return $intersection[0];
+        }
+    }
 }
--- tests/units/Langchecker/Utils.php
+++ tests/units/Langchecker/Utils.php
@@ -314,4 +314,33 @@ class Utils extends atoum\test
             ->string($obj->getCliParam(1, $options, 'foo'))
                 ->isEqualTo('test');
     }
+
+    public function testDetectLocale()
+    {
+        $obj = new _Utils();
+
+        $this
+            ->string($obj->detectLocale())
+                ->isEqualTo('en-US');
+
+        $this
+            ->string($obj->detectLocale([], 'en-GB'))
+                ->isEqualTo('en-GB');
+
+        $this
+            ->string($obj->detectLocale(['fr', 'it'], 'en-US', 'it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3'))
+                ->isEqualTo('it');
+
+        $this
+            ->string($obj->detectLocale(['ff', 'fr', 'it'], 'en-US', 'ff,fr-FR;q=0.8,fr;q=0.7,en-GB;q=0.5,en-US;q=0.3,en;q=0.2'))
+                ->isEqualTo('ff');
+
+        $this
+            ->string($obj->detectLocale(['fr', 'it'], 'en-US', 'ff,fr-FR;q=0.8,fr;q=0.7,en-GB;q=0.5,en-US;q=0.3,en;q=0.2'))
+                ->isEqualTo('fr');
+
+        $this
+            ->string($obj->detectLocale(['xh'], 'en-US', 'ff,fr-FR;q=0.8,fr;q=0.7,en-GB;q=0.5,en-US;q=0.3,en;q=0.2'))
+                ->isEqualTo('en-US');
+    }
 }
