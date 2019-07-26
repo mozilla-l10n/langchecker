b6c5ecd9a348c2251bd7cdfa1a017ef46ea5c0ef
Théo Chevalier <theo.chevalier11@gmail.com>
2019-07-26 11:01:46 +0200

    remove pl

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -833,7 +833,7 @@ $engagement_lang = [
         ],
     ],
     'emails/2019/fb_2fa_win.lang' => [
-        'supported_locales' => ['de', 'fr', 'pl'],
+        'supported_locales' => ['de', 'fr'],
     ],
     'emails/2019/fb_open_letter.lang' => [
         'supported_locales' => ['de', 'fr'],
