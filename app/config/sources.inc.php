e2667e3b92904779b5f4a1148835ab4c07880267
Théo Chevalier <theo.chevalier11@gmail.com>
2020-04-04 18:02:40 +0100

    Zoom graphics

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -1061,6 +1061,9 @@ $engagement_lang = [
     'other/2020/pni_security_standards_badge.lang' => [
         'supported_locales' => ['de', 'fr'],
     ],
+    'other/2020/zoom_graphics.lang' => [
+        'supported_locales' => ['de', 'fr'],
+    ],
     'social/2016/fundraising.lang' => [
         'supported_locales' => [
             'de', 'en-GB', 'es', 'fr', 'it', 'nl', 'pt-BR',
