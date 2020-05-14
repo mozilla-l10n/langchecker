1dc3fd6839d762f7738ce7e42a1500a517fe02d7
Craig Cook <craigcook@users.noreply.github.com>
2020-05-14 15:38:48 -0700

    Add firefox/whatsnew_77.lang

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -407,6 +407,14 @@ $mozillaorg_lang = [
         ],
         'supported_locales' => $firefox_locales,
     ],
+    'firefox/whatsnew_77.lang' => [
+        'deadline'          => '2020-05-22',
+        'priority'          => [
+            1 => ['de', 'fr', 'en-GB', 'en-CA'],
+            3 => ['all'],
+        ],
+        'supported_locales' => $firefox_locales,
+    ],
     'firefox/windows-64-bit.lang' => [
         'priority'          => 3,
         'supported_locales' => $firefox_locales,
