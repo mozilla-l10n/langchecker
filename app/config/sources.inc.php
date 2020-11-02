c75da729b1ddbfad5fc8c7e25379c632b00b8b2d
Théo Chevalier <theo.chevalier11@gmail.com>
2020-11-02 11:33:26 +0000

    Unfck (#1006)

--- app/config/sources.inc.php
+++ app/config/sources.inc.php
@@ -1065,6 +1065,9 @@ $engagement_lang = [
     'websites/foundation/homepage.lang' => [
         'supported_locales' => ['de', 'es', 'fr', 'fy-NL', 'nl', 'pl', 'pt-BR'],
     ],
+    'websites/foundation/unfck_the_internet.lang' => [
+        'supported_locales' => ['fr'],
+    ],
     'websites/mozfest/uniquely_mozfest.lang' => [
         'supported_locales' => ['de', 'fr', 'fy-NL', 'nl'],
     ],
